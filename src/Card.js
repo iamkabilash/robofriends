f35ad76dd77a9ef24c20b4365653fd1f75f7ad97
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-05-11 23:23:42 +0530

    starter files

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return(
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-2">
+            <img alt="robots" src="https://robohash.org/testtest" />
+            <div>
+                <h2>Kabilash</h2>
+                <p>iamkabilash@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
