4c5a2a3c57a27278750cfeb05f7df4b01776e6af
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-05-14 11:13:37 +0530

    added robots.js and props

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card name={robots[0].name} id={robots[0].id} email={robots[0].email} />
+    <Card name={robots[1].name} id={robots[1].id} email={robots[1].email} />
+    <Card name={robots[2].name} id={robots[2].id} email={robots[2].email} />
+    <Card name={robots[3].name} id={robots[3].id} email={robots[3].email} />
+    <Card name={robots[4].name} id={robots[4].id} email={robots[4].email} />
   </React.StrictMode>
 );
 
