ee4860c14ceb89d92de4309dec14e85d3920137c
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-05-14 12:13:55 +0530

    created cardList.js to display all robots

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import {robots} from './robots';
+import CardList from './CardList'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card name={robots[0].name} id={robots[0].id} email={robots[0].email} />
-    <Card name={robots[1].name} id={robots[1].id} email={robots[1].email} />
-    <Card name={robots[2].name} id={robots[2].id} email={robots[2].email} />
-    <Card name={robots[3].name} id={robots[3].id} email={robots[3].email} />
-    <Card name={robots[4].name} id={robots[4].id} email={robots[4].email} />
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
