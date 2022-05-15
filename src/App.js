ffbf635285f8886b25990b3ffd00b9a92d7f18c0
Kabilash Satheesh Kumar <iamkabilash@Kabilashs-MacBook-Pro.local>
2022-05-15 19:22:25 +0530

    added hooks, fixed title and searchbar on scroll, imported robots from json placeholder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import CardList from "./CardList";
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css'
+import Scroll from './Scroll'
 
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            robots: robots,
+            robots: [],
             searchfield: "",
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(Response => Response.json()).then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -26,7 +30,7 @@ class App extends React.Component {
             <div className="tc">
                 <h1>Robofriends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll><CardList robots={filteredRobots}/></Scroll>
             </div>
         );
     }
